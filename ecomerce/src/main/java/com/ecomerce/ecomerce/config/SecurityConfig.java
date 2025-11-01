fd9d7fd21036119b467ef434e0591267104f0ec2
Kenrro <kenrro2003@gmail.com>
2025-11-01 09:32:06 +0100

    Fix the problem with the dockerfile

--- ecomerce/src/main/java/com/ecomerce/ecomerce/config/SecurityConfig.java
+++ ecomerce/src/main/java/com/ecomerce/ecomerce/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ecomerce.ecomerce.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ecomerce.ecomerce.jwt.JwtAuthenticationFilter;
 
@@ -29,6 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
@@ -37,6 +43,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/families/**").hasAnyRole("ADMIN", "USER")
                 .requestMatchers("/api/families/**").hasRole("ADMIN")
                 .requestMatchers("/api/cart/**").permitAll()
+                .requestMatchers("/api/cart/**").hasAnyRole("ADMIN", "USER")
                 .anyRequest().authenticated()
             )
             .authenticationProvider(authenticationProvider)
@@ -58,5 +65,17 @@ public class SecurityConfig {
             );
             
         return http.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
